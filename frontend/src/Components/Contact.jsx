3e7798e4e54f1144e71d44b20f8559844194a811
Swechhya Maharjan <swekchya.mjn@gmail.com>
2026-01-27 19:05:21 +0545

    Fixed

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const res = await axios.post(
-        `${import.meta.env.VITE_API_URL}/sendContact`,
+        `${import.meta.env.VITE_API_URL}/api/sendContact`,
         formData,
         {
           headers: {
